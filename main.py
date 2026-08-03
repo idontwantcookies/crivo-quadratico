b2e66f47c97f473789efd4ec63f11bf2cc7738cf
Felipe Muniz <35815542+idontwantcookies@users.noreply.github.com>
2026-08-03 17:33:01 -0300

    Aumentando o máximo de dígitos

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def parse_int(raw: str) -> int:
 
 # ------------------------------------------------------- primalidade ------
 
-MR_MAX_DIGITS = 2000
+MR_MAX_DIGITS = 200
 
 
 @when("change", "#mr-preset")
